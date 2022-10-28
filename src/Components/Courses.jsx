389d750936ea615b822691294ed38090ef771bdd
AnitaAkter <108604188+AnitaAkter@users.noreply.github.com>
2022-10-28 18:14:33 +0600

    again committed

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -8,10 +8,12 @@ const Courses = () => {
     const detailsCourse = useLoaderData();
     return (
         <div className='grid grid-cols-6 bg-purple-300'>
-            <SideBar></SideBar>
+            <div className='col-span-2'>
+                <SideBar></SideBar>
+            </div>
             <div className='col-span-4'>
 
-                <section className="py-6 grid grid-cols-2 sm:py-12 dark:bg-gray-800">
+                <section className="py-6 grid lg:grid-cols-2 sm:grid-cols-1 sm:py-12 dark:bg-gray-800">
                     {
                         detailsCourse.map(detailsCourse => <DetailsCourse key={detailsCourse._id} detailsCourse={detailsCourse}></DetailsCourse>)
                     }
