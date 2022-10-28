2f4075fdcb43f99408757ebd36a4519b02e9df37
AnitaAkter <108604188+AnitaAkter@users.noreply.github.com>
2022-10-28 14:33:42 +0600

    commit

--- src/Components/PDFCourse.jsx
+++ src/Components/PDFCourse.jsx
@@ -20,14 +20,14 @@ const PDFCourse = () => {
         <section className="dark:bg-gray-800 dark:text-gray-100">
             <div id='downloadJsPDF' className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
 
-                <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
+                <Link rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
                     <img src={courseName.image_url} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
                     <div className="p-6 space-y-2 lg:col-span-5">
                         <h2 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">{courseName.title}</h2>
                         <p>{courseName.details}</p>
                         <button onClick={handlePDFCourse} className="bg-red-400 px-8 py-2 rounded text-black">Download PDF</button>
                     </div>
-                </a>
+                </Link>
             </div>
         </section>
     );
